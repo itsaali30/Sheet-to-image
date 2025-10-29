761bb304cc7dd5155ea46033fd5f072c9f9fed3e
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-10-30 02:58:29 +0530

    Add Vue plugin and set base path in Vite config
    Updated Vite configuration to include Vue plugin and set base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+    
+     base: '/Sheet-to-image/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
